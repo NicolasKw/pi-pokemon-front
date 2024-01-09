53ae9a68a245ed421d65cfda5c5d37eea9fa956a
Nicolas Kwiatkowski <nicolasm.kw@gmail.com>
2024-01-09 18:25:56 -0300

    Prueba con otras configuraciones para deploy

--- clientVite/vite.config.js
+++ clientVite/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Pi-Pokemon/"
+  base: "/PI-Pokemon/",
+  plugins: [react()]
 })
